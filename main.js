c2ed61d9dcee2cad51c56f3afd2e48d577705290
lidicedupuit <lidicedupuit2710@gmail.com>
2023-03-20 20:46:28 -0300

    mudança no js

--- main.js
+++ main.js
@@ -1,34 +1,12 @@
-const form = document.getElementById('form-confirm');
-const valA = document.getElementById('A');
-const valB = document.getElementById('B');
-const valor-A = valA;
-const valor-B = valB;
-let formValid =false;
+const submitButtonHtml = document.getElementById('submitButton');
+const valAHtml = document.getElementById ('valA');
+cosnt valBHtml = document.getElementById ('valB');
 
-function campoValid (valA, valB){
-    return valB > valA;
-}
-form.addEventListener('submit', function(e){
-e.preventDefault();
-
-const valA = parseFloat(document.getElementById('valor-A'.value));
-const valB = parseFloat(document.getElementById('valor-B'.value));
-const messageSuccess = `resultado correto o valaor de B:<b> ${valB.value} é maior que o valor de A ${valA.value}</b>`;
-const messageSuccess = `resultado errado de  B:<b> ${valB.value} é menor que o valor de A ${valB.value}</b>`;
-
-
-formValid = camporValid(valor-A.value, valor-B.value);
-if (formValid ) {
-    doccument.querySelector('.message-success').innerHTML = messagesucess;
-    alert(messageSuccess);
-
-    valor-A.value = '';
-    valor-B.value = '';
-} else {
-    
-        document.querySelector('.message-error').innerHTML = mensagemErro;
-        alert(mensagemErro);
-    
+submitButtonHtml.addEventListener('clique', function(e){
+    e. preventDefault ();
+    if (valAHtml.value < valBHtml.value) {
+        return document.getElementById('message').innerText = "o valor de B é maior do que o de A"
+    }else {
+        return document.getElementById('message').innerText = "o valor de A é maior do que o de B"
     }
-    
-})
+});
